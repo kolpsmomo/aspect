98b4ae2569b4c8e32945479e0f44891c26eb2960
denis0001-dev <denis0001.dev@ya.ru>
2025-05-01 21:33:46 +0300

    Начало интерфейса

--- app/src/main/java/com/aspectseries/myapplication/MainActivity.kt
+++ app/src/main/java/com/aspectseries/myapplication/MainActivity.kt
@@ -4,44 +4,56 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.aspectseries.myapplication.ui.theme.AspectTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AspectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+    @Composable
+    @Preview(showSystemUi = true, showBackground = true)
+    fun MainScreen() {
+        AspectTheme {
+            Scaffold { innerPadding ->
+                Column(Modifier.padding(innerPadding)) {
+                    Column {
+                        Text("column1")
+                        Text("column2")
+                        Text("column3")
+                        Text("column4")
+                        Text("column5")
+                        Text("column6")
+
+                        Button(onClick = {
+                            println("tewszdtestgserghsehgf;nersbuiseafb sr")
+                        }) {
+                            Text("test")
+                        }
+                    }
+
+                    Row {
+                        Text("row1")
+                        Text("row2")
+                        Text("row3")
+                        Text("row4")
+                        Text("row5")
+                        Text("row6")
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AspectTheme {
-        Greeting("Android")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MainScreen()
+        }
     }
 }
